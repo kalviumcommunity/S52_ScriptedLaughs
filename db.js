87740af776c2a63ea37d53bbf8c6a4df99f3f0d6
srikishore5727 <srikishore9080676683@gmail.com>
2024-02-06 14:10:39 +0530

    router file created and .env file created

--- db.js
+++ db.js
@@ -2,21 +2,21 @@ const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const startDatabase = async ()=>{
-    try{
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+const startDatabase = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("connected with mongoDB");
-    }catch(err){
-        console.log('Error : ',err.message);
+    } catch (err) {
+        console.log('Error : ', err.message);
     }
 };
 
-const stopDatabase = async()=>{
-    try{
+const stopDatabase = async () => {
+    try {
         await mongoose.disconnect();
         console.log('stopped');
-    }catch(err){
-        console.log('Error : ',err.message);
+    } catch (err) {
+        console.log('Error : ', err.message);
     }
 };
 
