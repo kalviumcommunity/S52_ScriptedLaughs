2d89b2acbdeb2a387ad9964d31a52f90af6aa40f
srikishore5727 <srikishore9080676683@gmail.com>
2024-02-07 14:56:31 +0530

    bruno file updated

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const startDatabase = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        const connection = await mongoose.connect(process.env.MONGO_URI);
         console.log("connected with mongoDB");
     } catch (err) {
         console.log('Error : ', err.message);
