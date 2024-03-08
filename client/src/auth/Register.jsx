81871bb0fb3568bbe703e2abd3a66c8c187df9c6
srikishore5727 <srikishore9080676683@gmail.com>
2024-03-08 10:54:25 +0530

    completed

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import './Register.css';
+import './register.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import * as yup from 'yup';
